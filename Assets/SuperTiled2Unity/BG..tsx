6f5d42782fcaa2b4fae108d1dec87fc011a85a74
Leon Bui <s2100950@edu.bc.fi>
2024-04-20 19:51:33 +0300

    toimiiko

--- Assets/SuperTiled2Unity/BG..tsx
+++ Assets/SuperTiled2Unity/BG..tsx
@@ -2,24 +2,24 @@
 <tileset version="1.10" tiledversion="1.10.2" name="BG" tilewidth="64" tileheight="64" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="64" height="64" source="../Downloads/Pixel Adventure 1/Free/Background/Blue.png"/>
+  <image source="../Downloads/Pixel Adventure 1/Free/Background/Blue.png"/>
  </tile>
  <tile id="1">
-  <image width="64" height="64" source="../Downloads/Pixel Adventure 1/Free/Background/Brown.png"/>
+  <image source="../Downloads/Pixel Adventure 1/Free/Background/Brown.png"/>
  </tile>
  <tile id="2">
-  <image width="64" height="64" source="../Downloads/Pixel Adventure 1/Free/Background/Gray.png"/>
+  <image source="../Downloads/Pixel Adventure 1/Free/Background/Gray.png"/>
  </tile>
  <tile id="3">
-  <image width="64" height="64" source="../Downloads/Pixel Adventure 1/Free/Background/Green.png"/>
+  <image source="../Downloads/Pixel Adventure 1/Free/Background/Green.png"/>
  </tile>
  <tile id="4">
-  <image width="64" height="64" source="../Downloads/Pixel Adventure 1/Free/Background/Pink.png"/>
+  <image source="../Downloads/Pixel Adventure 1/Free/Background/Pink.png"/>
  </tile>
  <tile id="5">
-  <image width="64" height="64" source="../Downloads/Pixel Adventure 1/Free/Background/Purple.png"/>
+  <image source="../Downloads/Pixel Adventure 1/Free/Background/Purple.png"/>
  </tile>
  <tile id="6">
-  <image width="64" height="64" source="../Downloads/Pixel Adventure 1/Free/Background/Yellow.png"/>
+  <image source="../Downloads/Pixel Adventure 1/Free/Background/Yellow.png"/>
  </tile>
 </tileset>
